76cf8b86d153b78e597f077e89ca5f7a07f712cb
MattBell <mattjbell97@gmail.com>
2019-04-02 11:51:45 -0400

    physics update

--- Dusk/src/Physics.cpp
+++ Dusk/src/Physics.cpp
@@ -3,10 +3,39 @@
 
 Physics::Physics()
 {
+	black.r = 0;
+	black.g = 0;
+	black.b = 0;
+
+	white.r = 255;
+	white.g = 255;
+	white.b = 255;
+
 }
 
 Physics::~Physics()
 {
+}
+
+void Physics::setMovementMap(ofImage * map)
+{
+	pixels = map->getPixels();
+
+	for (int j = 0; j < map->getHeight(); j++)
+	{
+		for (int i = 0; i < map->getWidth(); i++)
+		{		
+			c = pixels.getColor(j,i);
+			if (c == black)
+			{
+				movementMap[j][i] = 1;
+			}
+			else if (c == white)
+			{
+				movementMap[j][i] = 0;
+			}
+		}
+	}
 }
 
 //int Physics::checkDistance(objectX, objectY)
--- Dusk/src/Physics.h
+++ Dusk/src/Physics.h
@@ -3,11 +3,19 @@
 class Physics
 {
 public:
-	ofPixels movementCollisionMap;
-	ofPixels arrowCollisionMap;
+	//ofPixels movementCollisionMap;
+	//ofPixels arrowCollisionMap;
 
 	Physics();
 	~Physics();
+
+	int movementMap[mapHeight][mapWidth];
+	ofColor black;
+	ofColor white;
+	ofColor c;
+	ofPixels pixels;
+
+	void setMovementMap(ofImage * map);
 
 	//int checkDistance(objectX, objectY);
 	//int getAngle(objectX, objectY);
